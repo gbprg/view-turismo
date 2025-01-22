3a5a600bf1dc2fc3424fa9d39065fe9173e19a98
gbprg <duarte.gabrielgbd@gmail.com>
2025-01-22 20:00:02 -0300

    fix: edit next config for images

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"],
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { TourList } from "@/components/tourList";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold text-center my-8">Nossos Tours</h1>
+      <h1 className="text-3xl font-bold text-center my-8">Nossos Passeios</h1>
       <TourList />
     </main>
   );
